145eb7e70e1663ba1b767daa0e1e3c4991f5e1b7
Pedro Lima <pedro.lima@b2card.com.br>
2022-02-10 14:43:31 -0300

    Upload project initial

---
+++ Application.Interfaces/Services/Domain/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces.Services.Standard;
+using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services.Domain
+{
+    public interface IUserService : IServiceBase<User>
+    {
+        Task<IEnumerable<User>> GetAllIncludingTasksAsync();
+        Task<User> GetByIdIncludingTasksAsync(int id);
+    }
+}
